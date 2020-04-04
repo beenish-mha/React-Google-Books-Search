1eba9fd2a94eabeb20850a147ff616af78e35ab8
beenish <beeni.khurram@gmail.com>
2020-04-04 10:18:08 +0100

    connecting db with mlab

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ const mongoose = require("mongoose");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/book", {
-  useNewUrlParser: true,
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://heroku_75jkfncr:BeemLab5@ds331758.mlab.com:31758/heroku_75jkfncr",
+  {
+    useNewUrlParser: true,
+  }
+);
+
+mongoose.Promise = global.Promise;
 
 app.use("/book", bookRouter);
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const app = require("./app");
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const server = http.createServer(app);
 server.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
